7b6befd631ec0e9ce6e8f8311c7d9ece219fe07a
Mahadi Hasan <mahadih137@gmail.com>
2023-11-05 01:42:55 +0600

    login page created

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,63 +4,61 @@ import { GiSpellBook } from "react-icons/gi";
 
 const MainLayout = () => {
   return (
-    <div>
-      <div className="drawer">
-        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col">
-          {/* Navbar */}
-          <div className="w-full bg-base-300">
-            <div className="flex items-center max-w-7xl mx-auto px-6">
-              <div className="flex-none lg:hidden">
-                <label
-                  htmlFor="my-drawer-3"
-                  aria-label="open sidebar"
-                  className="btn btn-square btn-ghost"
+    <div className="drawer">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col">
+        {/* Navbar */}
+        <div className="w-full bg-base-300">
+          <div className="flex items-center w-full max-w-7xl mx-auto px-6">
+            <div className="flex-none lg:hidden">
+              <label
+                htmlFor="my-drawer-3"
+                aria-label="open sidebar"
+                className="btn btn-square btn-ghost"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block w-6 h-6 stroke-current"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    className="inline-block w-6 h-6 stroke-current"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    ></path>
-                  </svg>
-                </label>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
+            </div>
+            <div className="logo flex-1 px-2 mx-2 font-bold text-xl">
+              <div className="flex gap-3 items-center">
+                <GiSpellBook className="text-4xl -mt-2" />
+                Encyclopaedia
               </div>
-              <div className="logo flex-1 px-2 mx-2 font-bold text-xl">
-                <div className="flex gap-3 items-center">
-                  <GiSpellBook className="text-4xl -mt-2" />
-                  Encyclopaedia
-                </div>
-              </div>
-              <div className="flex-none hidden lg:block">
-                <div className="menu menu-horizontal font-semibold text-base">
-                  {/* Navbar menu content here */}
-                  <Navbar />
-                </div>
+            </div>
+            <div className="flex-none hidden lg:block">
+              <div className="menu menu-horizontal font-semibold text-base">
+                {/* Navbar menu content here */}
+                <Navbar />
               </div>
             </div>
           </div>
-          {/* Page content here */}
-          <div className="mx-auto max-w-7xl px-6">
-            <Outlet />
-          </div>
         </div>
-        <div className="drawer-side">
-          <label
-            htmlFor="my-drawer-3"
-            aria-label="close sidebar"
-            className="drawer-overlay"
-          ></label>
-          <div className="menu p-4 w-80 min-h-full bg-base-200 text-lg font-semibold pt-6">
-            {/* Sidebar content here */}
-            <Navbar />
-          </div>
+        {/* Page content here */}
+        <div className="mx-auto w-full max-w-7xl px-6">
+          <Outlet />
+        </div>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-3"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <div className="menu p-4 w-80 min-h-full bg-base-200 text-lg font-semibold pt-6">
+          {/* Sidebar content here */}
+          <Navbar />
         </div>
       </div>
     </div>
