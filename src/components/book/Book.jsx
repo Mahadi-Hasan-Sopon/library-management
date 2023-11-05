275a3a0c1ca92475d4433de1b2cf8a369440e08e
Mahadi Hasan <mahadih137@gmail.com>
2023-11-05 18:49:38 +0600

    book and all book component updated

--- src/components/book/Book.jsx
+++ src/components/book/Book.jsx
@@ -1,17 +1,29 @@
 import PropTypes from "prop-types";
+import Rating from "react-rating";
+import { AiFillStar } from "react-icons/ai";
 
 const Book = ({ book }) => {
-  const { title, image, author, category } = book || {};
+  const { title, image, author, category, rating } = book || {};
   return (
     <div>
-      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+      <div className="flex flex-col bg-base-200 shadow-xl rounded">
         <figure>
-          <img src={image?.cover} alt="Shoes" />
+          <img className="w-full h-96" src={image?.cover} alt={title} />
         </figure>
-        <div className="card-body">
+        <div className="flex flex-col py-3">
           <h2 className="card-title">{title} </h2>
           <p className="text-lg font-medium">Category: {category} </p>
           <p className="text-base font-medium">Author: {author} </p>
+          <div className="ratting">
+            <Rating
+              initialRating={rating?.rate}
+              readonly
+              emptySymbol={<AiFillStar className="text-2xl text-white" />}
+              fullSymbol={<AiFillStar className="text-2xl text-orange-600" />}
+              fractions={5}
+            />
+          </div>
+          <div className="flex-1"></div>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">See Details</button>
           </div>
--- src/pages/allBook/AllBook.jsx
+++ src/pages/allBook/AllBook.jsx
@@ -1,16 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAllBook } from "../../api/Api";
+import LoadingSpinner from "../../utils/LoadingSpinner";
+import Book from "../../components/book/Book";
 
 const AllBook = () => {
-  const allBook = useQuery({
+  const {
+    data: allBook,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
     queryKey: ["allBook"],
     queryFn: getAllBook,
-    initialData: [],
+    refetchOnWindowFocus: false,
+    staleTime: 5000 * 60,
   });
 
-  console.log(allBook.data);
+  if (isLoading) return <LoadingSpinner />;
 
-  return <div> {""} </div>;
+  return (
+    <div>
+      {isError && error?.message}
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-20">
+        {allBook?.map((book) => (
+          <Book book={book} key={book._id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllBook;
