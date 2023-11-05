4cbb79fe96fe9765f8eae1639bd7193af8c9574e
Mahadi Hasan <mahadih137@gmail.com>
2023-11-05 16:39:34 +0600

    tanstack query installed with basic setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,20 @@ import { RouterProvider } from "react-router-dom";
 import routes from "./routes/Routes.jsx";
 import ThemeContextProvider from "./contexts/ThemeContextProvider.jsx";
 import AuthContextProvider from "./contexts/AuthContextProvider.jsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeContextProvider>
       <AuthContextProvider>
-        <RouterProvider router={routes} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={routes} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
         <Toaster />
       </AuthContextProvider>
     </ThemeContextProvider>
