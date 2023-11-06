762f45ed00627e14220155950e2e8543c31da749
Mahadi Hasan <mahadih137@gmail.com>
2023-11-07 03:00:20 +0600

    borrowed book return functionality added

--- src/pages/borrowedBooks/BorrowedBooks.jsx
+++ src/pages/borrowedBooks/BorrowedBooks.jsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import { getBooksIdByEmail, getBorrowedBooksById } from "../../api/Api";
 import useAuth from "../../hooks/useAuth";
 import LoadingSpinner from "../../utils/LoadingSpinner";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const BorrowedBooks = () => {
   const { user } = useAuth();
@@ -42,9 +44,43 @@ const BorrowedBooks = () => {
     return formatDate(specificBook?.createdAt);
   };
 
+  const handleReturnClick = (id, quantity) => {
+    console.log("id: ", id, "quantity: ", quantity);
+    try {
+      axios
+        .delete(
+          `http://localhost:5000/allBook?email=${user?.email}&bookId=${id}`
+        )
+        .then((res) => {
+          console.log(res.data);
+          if (res.data.deletedCount > 0) {
+            axios
+              .patch(`http://localhost:5000/allBook/${id}`, {
+                quantity: parseInt(quantity) + 1,
+              })
+              .then((res) => {
+                console.log(res.data);
+                if (res.data.modifiedCount > 0) {
+                  toast.success("Book returned Successfully. Keep Reading!");
+                }
+              })
+              .catch((err) => {
+                console.log(err);
+                toast.error(err?.message);
+              });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="py-10 md:py-20">
-      <h1 className="text-2xl md:text-3xl font-bold text-center">
+      <h1 className="text-2xl md:text-3xl font-bold text-center mb-10">
         Borrowed Books List
       </h1>
       <div className="borrowedBooks grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
@@ -68,7 +104,10 @@ const BorrowedBooks = () => {
                 </div>
               </div>
               <div className="flex-grow mb-4"></div>
-              <button className="block w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base sm:w-auto px-6 md:px-10 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:disabled:bg-gray-600 disabled:bg-gray-500 disabled:text-gray-400">
+              <button
+                onClick={() => handleReturnClick(book._id, book?.quantity)}
+                className="block w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base sm:w-auto px-6 md:px-10 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:disabled:bg-gray-600 disabled:bg-gray-500 disabled:text-gray-400"
+              >
                 Return
               </button>
             </div>
@@ -86,5 +125,7 @@ const formatDate = (bookDate) => {
   const day = date.getDate();
   const month = date.getMonth() + 1; // Month is zero-based, so we add 1
   const year = date.getFullYear();
-  return `${day}-${month}-${year}`;
+  return `${day <= 9 ? `0${day}` : day}-${
+    month <= 9 ? `0${month}` : month
+  }-${year}`;
 };
